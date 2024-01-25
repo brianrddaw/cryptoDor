e712a8df396e8ef854d319d59ac80ffc5e8f0b9a
brianrddaw <brianramirezdaw@gmail.com>
2024-01-25 17:49:36 +0100

    carpetas organizadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './crypto/pages/dashboard.component';
 import { CryptosComponent } from './crypto/pages/cryptos/cryptos.component';
+import { MainComponent } from './crypto/pages/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DashboardComponent, CryptosComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    DashboardComponent,
+    CryptosComponent,
+    MainComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
