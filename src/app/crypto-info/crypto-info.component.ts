21cc779426d98fd4dcda7c3b2eddd6a289a294d6
diego_c <curbelord@gmail.com>
2024-01-29 10:23:25 +0000

    Parte Cryptos - Diego

---
+++ src/app/crypto-info/crypto-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './crypto-info.component.html',
+  styleUrl: './crypto-info.component.css'
+})
+export class CryptoInfoComponent {
+
+}
