242d41a08199e29dca8ed2c40009eb9021882aa7
brianrddaw <brianramirezdaw@gmail.com>
2024-01-31 15:11:22 +0100

    cambios en el css: main, cryptos, prices; mejora de los section en la main page - Brian

--- src/app/crypto/components/main-tri-a/main-tri-a.component.ts
+++ src/app/crypto/components/main-tri-a/main-tri-a.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-main-tri-a',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './main-tri-a.component.html',
   styleUrl: './main-tri-a.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
