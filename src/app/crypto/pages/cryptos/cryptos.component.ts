6a0e9a4c729c7813f938af6e35ee85349d69739e
mariomague <mariomague123@gmail.com>
2024-02-02 16:53:02 +0000

    Ya furula el coger datos

--- src/app/crypto/pages/cryptos/cryptos.component.ts
+++ src/app/crypto/pages/cryptos/cryptos.component.ts
@@ -21,6 +21,9 @@ export class CryptosComponent {
   diccionario: any = [];
   cryptoName: string = '';
   crytoImg: any = '';
+  cryptoDescription: any = '';
+  cryptoId: any = '';
+  cryptoPosition: any = '';
 
   constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {
     this.getCryptoInfo();
@@ -35,6 +38,7 @@ export class CryptosComponent {
             this.diccionario.push({
               img: data['coins'][i]['item']['large'],
               name: data['coins'][i]['item']['name'],
+              id: data['coins'][i]['item']['id'],
             });
 
             this.cdr.detectChanges();
@@ -43,4 +47,47 @@ export class CryptosComponent {
         (error) => console.error(error)
       );
   }
+
+
+
+
+  cryptoSearch(cryptoName?: string): void {
+
+    if (cryptoName) {
+      console.log(cryptoName);
+      this.cryptoName = cryptoName;
+    } else {
+      const input = document.getElementById('cryptos_search_input') as HTMLInputElement;
+      this.cryptoName = input.value;
+    }
+    
+
+    if(this.cryptoName) {
+      console.log(this.cryptoName);
+
+      // Obtener datos de la API
+      
+      this.http.get<any>(`https://api.coingecko.com/api/v3/coins/${this.cryptoName}`).subscribe(
+        data => {
+          // console.log(data);
+          const { image: { small }, name, symbol, market_cap_rank, description } = data;
+          this.cryptoName = name;
+          this.crytoImg = small;
+          this.cryptoDescription = description;
+          this.cryptoId = symbol;
+          this.cryptoPosition = market_cap_rank;
+          this.cdr.detectChanges();
+          console.log(this.cryptoName);
+          console.log(this.crytoImg);
+          console.log(this.cryptoDescription.en);
+          console.log(this.cryptoId);
+          console.log(this.cryptoPosition);
+        }
+      )
+    } else {
+      console.log('No se ha introducido un nombre de criptomoneda');
+    }
+  }
+
+
 }
--- src/app/crypto/pages/prices/prices.component.ts
+++ src/app/crypto/pages/prices/prices.component.ts
@@ -2,13 +2,80 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { DashboardComponent } from '../dashboard.component';
 import { FooterComponent } from '../main/main-pages/footer/footer.component';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-prices',
   standalone: true,
-  imports: [CommonModule, DashboardComponent, FooterComponent],
+  imports: [CommonModule, 
+     DashboardComponent,
+     FooterComponent,
+     FormsModule,
+    ],
   templateUrl: './prices.component.html',
   styleUrls: ['./prices.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PricesComponent {}
+export class PricesComponent {
+  // Declaraciones
+  cryptoName: string = '';
+  cryptoPrice: number = 0;
+  cryptoImage: string = '';
+  cryptoSymbol: string = '';
+  cryptoMarketPosition: number = 0;
+  cryptoPriceChange: number = 0;
+
+  cryptoPriceChange24h: number = 0;
+  cryptoPriceChange7d: number = 0;
+  cryptoPriceChange14d: number = 0;
+  cryptoPriceChange30d: number = 0;
+  cryptoPriceChange60d: number = 0;
+  cryptoPriceChange1y: number = 0;
+
+
+
+  constructor(private http: HttpClient) {} // Inject HttpClient module
+
+  searchCrypto(): void {
+    if(this.cryptoName) {
+      console.log(this.cryptoName);
+
+      // Cogemos los datos de la api con https://api.coingecko.com/api/v3/coins/{id}
+      this.http.get<any>(`https://api.coingecko.com/api/v3/coins/${this.cryptoName}`).subscribe(
+        data => {
+          console.log(data);
+          const { image: { small }, name, symbol, market_cap_rank, market_data: { current_price, price_change_percentage_24h_in_currency, price_change_percentage_7d_in_currency, price_change_percentage_14d_in_currency, price_change_percentage_30d_in_currency, price_change_percentage_60d_in_currency, price_change_percentage_1y_in_currency } } = data;
+          this.cryptoName           = name;
+          this.cryptoPrice          = current_price.eur;
+          this.cryptoImage          = small;
+          this.cryptoSymbol         = symbol;
+          this.cryptoMarketPosition = market_cap_rank;
+          this.cryptoPriceChange24h = (price_change_percentage_24h_in_currency.eur/100) * this.cryptoPrice;
+          this.cryptoPriceChange7d  = (price_change_percentage_7d_in_currency.eur/100) * this.cryptoPrice;
+          this.cryptoPriceChange14d = (price_change_percentage_14d_in_currency.eur/100) * this.cryptoPrice;
+          this.cryptoPriceChange30d = (price_change_percentage_30d_in_currency.eur/100) * this.cryptoPrice;
+          this.cryptoPriceChange60d = (price_change_percentage_60d_in_currency.eur/100) * this.cryptoPrice;
+          this.cryptoPriceChange1y  = (price_change_percentage_1y_in_currency.eur/100) * this.cryptoPrice;
+          console.log(this.cryptoName);
+          console.log(this.cryptoPrice);
+          console.log(this.cryptoImage);
+          console.log(this.cryptoSymbol);
+          console.log(this.cryptoMarketPosition);
+          console.log(this.cryptoPriceChange24h);
+          console.log(this.cryptoPriceChange7d);
+          console.log(this.cryptoPriceChange14d);
+          console.log(this.cryptoPriceChange30d);
+          console.log(this.cryptoPriceChange60d);
+          console.log(this.cryptoPriceChange1y);
+        }
+      )
+      
+
+
+    } else {
+      console.log('No crypto name entered');
+    }
+  }
+
+}
