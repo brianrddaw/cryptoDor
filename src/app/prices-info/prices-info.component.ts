f492aaa7ab20185463432039f943b568307f1e97
Bladimir Peralta Herrera <bladimir.peralta@iescesarmanrique.com>
2024-02-05 15:30:00 +0000

    price-info implementado: Blady

---
+++ src/app/prices-info/prices-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prices-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './prices-info.component.html',
+  styleUrl: './prices-info.component.css'
+})
+export class PricesInfoComponent {
+
+}
